42fafe75d948ea8a1e5354d13344111a1cb28fde
Gilissen Steve <steve@pulseconsulting.be>
2019-06-24 15:03:29 +0200

    Added folder creation

--- convert_to_mp3.sh
+++ convert_to_mp3.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+OUTDIR="MP3"
+if [ -d "$OUTDIR" ]; then 
+	echo "Folder exists. Proceeding..."
+else
+	echo "Folder does not exist. Creating..."
+	mkdir -p "$OUTDIR"
+fi
 for f in *.{mod,MOD,xm,XM,it,IT,s3m,S3M};
 do
 	echo "Processing file $f..."
-	xmp $f -m -c | ffmpeg -f s16le -i pipe: -b:a 192k $f.mp3
+	xmp $f -m -c | ffmpeg -f s16le -i pipe: -b:a 192k $OUTDIR/$f.mp3
 done
